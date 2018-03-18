f2aa1cca41b31cc5a871ea8cbda1e1d541c95b6f
gregorsmith93 <gregors93@gmail.com>
2018-03-18 22:55:35 +0000

    Added documentation to configuration loading classes

--- WebFront.Config/src/main/java/com/webfront/config/ConfigurationLoader.java
+++ WebFront.Config/src/main/java/com/webfront/config/ConfigurationLoader.java
@@ -1,9 +1,20 @@
+/**
+ * @author Gregor Smith - 2018
+ */
 package com.webfront.config;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * Loads configuration properties.
+ */
 @Component
 public interface ConfigurationLoader {
 
+	/**
+	 * Retrieve context path for API.
+	 *
+	 * @return Context path.
+	 */
 	String getWebBackContextPath();
 }
--- WebFront.Config/src/main/java/com/webfront/config/ConfigurationLoaderHandler.java
+++ WebFront.Config/src/main/java/com/webfront/config/ConfigurationLoaderHandler.java
@@ -1,16 +1,29 @@
+/**
+ * @author Gregor Smith - 2018
+ */
 package com.webfront.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+/**
+ * Implementation of {@link ConfigurationLoader}
+ */
 @Component
 @PropertySource("classpath:webfront.properties")
 public class ConfigurationLoaderHandler implements ConfigurationLoader {
 
+	/**
+	 * Internal context path.
+	 */
 	@Value("${webback.context.path}")
 	private String webBackContextPath;
 
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
 	public String getWebBackContextPath() {
 		return webBackContextPath;
 	}
